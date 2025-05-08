067e134ddebc11c68c49f47d2bb3ad69e40cbe6b
teodord25 <djuric.teodor25@gmail.com>
2025-05-08 17:56:27 +0200

    update flake with wgpu nix shell stuff

--- flake.nix
+++ flake.nix
@@ -38,29 +38,70 @@
             };
       };
 
-      devShells = forEachSupportedSystem ({ pkgs }: {
+      devShells = forEachSupportedSystem ({ pkgs }: let
+        myInputs = with pkgs; [
+          rustToolchain
+          openssl
+          pkg-config
+          cargo-deny
+          cargo-edit
+          cargo-watch
+          rust-analyzer
+
+          # necessary for building wgpu in 3rd party packages (in most cases)
+          libxkbcommon
+          wayland xorg.libX11 xorg.libXcursor xorg.libXrandr xorg.libXi
+          alsa-lib
+          fontconfig freetype
+          shaderc directx-shader-compiler
+          pkg-config cmake
+          mold # could use any linker, needed for rustix (but mold is fast)
+
+          libGL
+          vulkan-headers vulkan-loader
+          vulkan-tools vulkan-tools-lunarg
+          vulkan-extension-layer
+          vulkan-validation-layers # don't need them *strictly* but immensely helpful
+
+          # necessary for developing (all of) wgpu itself
+          cargo-nextest cargo-fuzz
+
+          # nice for developing wgpu itself
+          typos 
+
+          yq # for tomlq below
+
+          # nice tools
+          gdb rr
+          evcxr
+          valgrind
+          renderdoc
+        ];
+
+        libPaths = builtins.toString (pkgs.lib.makeLibraryPath myInputs);
+
+      in {
         default = pkgs.mkShell {
-          packages = with pkgs; [
-            rustToolchain
-            openssl
-            pkg-config
-            cargo-deny
-            cargo-edit
-            cargo-watch
-            rust-analyzer
-            wayland            # provides libwayland-client.so
-            wayland-protocols  # often needed at build time
-          ];
+          buildInputs = myInputs;
+
+          LD_LIBRARY_PATH = libPaths;
 
           env = {
-            # Required by rust-analyzer
             RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
           };
 
           shellHook = ''
+            export RUSTC_VERSION="$(tomlq -r .toolchain.channel rust-toolchain.toml)"
+            export PATH="$PATH:''${CARGO_HOME:-~/.cargo}/bin"
+            export PATH="$PATH:''${RUSTUP_HOME:-~/.rustup/toolchains/$RUSTC_VERSION-x86_64-unknown-linux/bin}"
+            export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${libPaths}";
+
+            rustup default $RUSTC_VERSION
+            rustup component add rust-src rust-analyzer
+
             alias ga='git add'
             alias gs='git status'
-            alias ga='git commit'
+            alias gc='git commit'
             alias gp='git push'
           '';
         };
